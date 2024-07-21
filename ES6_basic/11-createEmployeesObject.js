0a298cf54357c0c5fa9fa91029a015d239f056a5
IsmaelBuere <7674@holbertonstudents.com>
2024-07-21 15:52:40 -0700

    task 11

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return {
-    [departmentName]: employees
+  const myObject = {
+    [departmentName]: [...employees],
   };
+  return myObject;
 }
