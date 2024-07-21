069560117f8c57900b09526a3419dd4c88af0872
IsmaelBuere <7674@holbertonstudents.com>
2024-07-21 16:05:03 -0700

    task 0

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,12 +1,12 @@
 export default function createReportObject(employeesList) {
-  const myObject = {
-  allEmployees: employeesList,
-  getNumberOfDepartments(employeesList) {
-    const keys = Object.keys(employeesList);
-    return keys.length;
-},
-
-  };
-
-  return myObject;
-}
+    const myObject = {
+      allEmployees: employeesList,
+      getNumberOfDepartments(employeesList) {
+        const keys = Object.keys(employeesList);
+        return keys.length;
+      },
+  
+    };
+  
+    return myObject;
+  }
